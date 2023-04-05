9ef6ecfc751e7981a123992b00202b42f46b7ebb
dongjun2000 <dongjun@246ha.com>
2023-04-05 16:08:59 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-start">
+      由 <a href="#" target="_blank">MyBBS</a> 设计和编码
+      <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-end">
+      <a href="mailto:admin@246ha.com">联系我们</a>
+    </p>
+  </div>
+</footer>
