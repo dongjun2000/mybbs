f7a289674753774e03a3f6dab3484e73985e17e0
dongjun2000 <dongjun@246ha.com>
2023-04-08 20:52:29 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,12 @@ use App\Models\Topic;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
         // 读取分类 ID 关联的话题
-        $topics = Topic::with(['user', 'category'])->where('category_id', $category->id)->paginate(20);
+        $topics = $topic->withOrder($request->order)
+            ->where('category_id', $category->id)
+            ->with('user', 'category')->paginate(20);
 
         return view('topics.index', compact('topics', 'category'));
     }
