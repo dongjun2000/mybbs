e12e5a6382a9346229673691b0009a57640730e5
dongjun2000 <dongjun@246ha.com>
2023-04-09 19:51:50 +0800

    用户可以删除自己发的话题

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -41,15 +41,21 @@
             {!! $topic->body !!}
           </div>
 
-          <div class="operate">
-            <hr>
-            <a href="{{ route('topics.edit', $topic) }}" class="btn btn-outline-secondary btn-sm" role="button">
-              <i class="far fa-edit"></i> 编辑
-            </a>
-            <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
-              <i class="far fa-trash-alt"></i> 删除
-            </a>
-          </div>
+          @can('update', $topic)
+            <div class="operate">
+              <hr>
+              <a href="{{ route('topics.edit', $topic) }}" class="btn btn-outline-secondary btn-sm" role="button">
+                <i class="far fa-edit"></i> 编辑
+              </a>
+              <form action="{{ route('topics.destroy', $topic->id) }}" style="display: inline-block;" method="post" onsubmit="return confirm('您确定要删除吗？')">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-outline-secondary btn-sm">
+                  <i class="far fa-trash-alt"></i> 删除
+                </button>
+              </form>
+            </div>
+          @endcan
         </div>
       </div>
     </div>
