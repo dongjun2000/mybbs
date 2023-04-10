4aede303345a798e31b676f66fbb8939897f5b21
dongjun2000 <dongjun@246ha.com>
2023-04-10 12:20:14 +0800

    富文本编辑器中可以上传图片

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Handlers\ImageUploadHandler;
 
 class TopicsController extends Controller
 {
@@ -68,5 +69,26 @@ class TopicsController extends Controller
         $topic->delete();
 
         return redirect()->route('topics.index')->with('success', '成功删除！');
+    }
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        $data = [
+            'success' => false,
+            'msg' => '上传失败!',
+            'file_path' => ''
+        ];
+
+        if ($file = $request->upload_file) {
+            $result = $uploader->save($file, 'topics', Auth::id(), 1024);
+
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg'] = '上传成功！';
+                $data['success'] = true;
+            }
+        }
+
+        return $data;
     }
 }
